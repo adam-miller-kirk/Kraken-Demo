4f2c02f777c158f823a919167b94112f892e2009
Adam Miller-Kirk <adam_miller-kirk@hotmail.com>
2025-04-07 12:46:42 +0100

    feature base loader and error components with simple product images setup

--- client/pages/product/index.tsx
+++ client/pages/product/index.tsx
@@ -1,7 +1,35 @@
+"use client";
+import Image from "next/image";
 import useProduct from "../../hooks/useProduct";
+import Loading from "../../components/loading";
+import Error from "../../components/error";
 
 export default function Product() {
   const { product, loading, error } = useProduct({ productId: 1 });
-  console.log(product, loading, error);
-  return <div>Product page</div>;
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (!product) return <p>Product not found.</p>;
+
+  return (
+    <div className="product">
+      <div className="title">
+        <Image
+          className="logo"
+          src="/octopus-logo.svg"
+          alt="Octopus Energy Logo"
+          width={100}
+          height={50}
+        />
+        <Image src="/basket.svg" alt="Basket Icon" width={20} height={20} />
+      </div>
+      <Image
+        className="image"
+        src="/philips-plumen.jpg"
+        alt="Philips Plumen Image"
+        width={300}
+        height={300}
+      />
+    </div>
+  );
 }
